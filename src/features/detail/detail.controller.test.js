be1855eea3c984e270c2a670ade0162b7ad5d46c
ericmasiello <eric.j.masiello@gmail.com>
2015-11-08 20:40:03 -0500

    Add test for detail controller

---
+++ src/features/detail/detail.controller.test.js
@@ -0,0 +1,17 @@
+import detail from './index';
+
+describe('Controller: Detail', function () {
+  let $controller;
+
+  beforeEach(angular.mock.module(detail));
+
+  beforeEach(angular.mock.inject(function (_$controller_) {
+    $controller = _$controller_;
+  }));
+
+  it('should set test value', function () {
+    let ctrl = $controller('DetailController');
+
+    expect(ctrl.test).toBe('testing');
+  });
+});
