d260f47d64ed87c70ed21a0dbb613b4ca59cac3a
ericmasiello <eric.j.masiello@gmail.com>
2015-11-20 07:16:13 -0500

    Setup tests for about services

--- src/services/about.service.test.js
+++ src/services/about.service.test.js
@@ -1,14 +1,31 @@
 import aboutServices from './about.service'
+//import app from '../app';
 
 describe('Services: About Model', function () {
   let service;
   let httpBackend;
+  let authRequestHandler;
 
   beforeEach(angular.mock.module(aboutServices));
+
+  beforeEach(function() {
+
+    //module()
+
+    module(function($provide) {
+      $provide.constant('routesPaths', {
+      });
+    });
+
+  });
 
   beforeEach(inject(function (_AboutModel_, $httpBackend) {
     service = _AboutModel_;
     httpBackend = $httpBackend;
+
+    authRequestHandler = $httpBackend.when('GET', '/auth.py')
+      .respond({userId: 'userX'}, {'A-Token': 'xxx'});
+
   }));
 
   afterEach(()=>{
