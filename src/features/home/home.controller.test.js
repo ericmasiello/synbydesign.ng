476810564087b11b2e6645be2fedced4d188e06e
ericmasiello <eric.j.masiello@gmail.com>
2015-12-19 16:15:43 -0500

    Remove references to home.css
    Update home.controller.test

--- src/features/home/home.controller.test.js
+++ src/features/home/home.controller.test.js
@@ -1,22 +1,37 @@
-//import home from './index';
-//
-//describe('Controller: Home', function () {
-//  let $controller;
-//
-//  beforeEach(angular.mock.module(home));
-//
-//  beforeEach(angular.mock.inject(function (_$controller_) {
-//    $controller = _$controller_;
-//  }));
-//
-//  it('name is initialized to World', function () {
-//    let ctrl = $controller('HomeController', {
-//      'randomNames': 'random name',
-//      'aboutContent': {
-//        content: 'Lorem ipsum'
-//      }
-//    });
-//
-//    expect(ctrl.name).toBe('World');
-//  });
-//});
+import home from './index';
+
+describe('Controller: Home', function () {
+  let ctrl;
+
+  beforeEach(angular.mock.module(home));
+
+  beforeEach(angular.mock.inject(function (_$controller_) {
+    'use strict';
+    ctrl = _$controller_('HomeController', {
+      'aboutContent': {
+        content: 'Lorem ipsum'
+      },
+      'portfolioContent': {
+        web: ['web item'],
+        other: ['other item'],
+        design: ['design item']
+      }
+    });
+  }));
+
+  afterEach(function(){
+    'use strict';
+    ctrl = undefined;
+  });
+
+  it('model is initialized with values', function () {
+
+    expect(ctrl.model).toBeDefined();
+    expect(ctrl.model.mastheadId).toEqual('masthead');
+    expect(ctrl.model.logoId).toEqual('logo');
+    expect(ctrl.model.about).toEqual('Lorem ipsum');
+    expect(ctrl.model.webPortfolio).toEqual(['web item']);
+    expect(ctrl.model.otherPortfolio).toEqual(['other item']);
+    expect(ctrl.model.designPortfolio).toEqual(['design item']);
+  });
+});
