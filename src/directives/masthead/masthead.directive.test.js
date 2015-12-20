34c9b276f4e9293af1a0697b58d96ce150d54d8a
ericmasiello <eric.j.masiello@gmail.com>
2015-12-20 16:17:39 -0500

    Finish unit tests

---
+++ src/directives/masthead/masthead.directive.test.js
@@ -0,0 +1,50 @@
+import directive from './masthead.directive';
+
+describe('Directive: masthead', function () {
+  let $compile;
+  let scope;
+
+  beforeEach(angular.mock.module(directive));
+
+  beforeEach(angular.mock.inject(function (_$compile_, _$rootScope_) {
+    'use strict';
+    $compile = _$compile_;
+    scope = _$rootScope_.$new();
+  }));
+
+  afterEach(function(){
+    'use strict';
+    scope = undefined;
+    $compile = undefined;
+  });
+
+  it('should set the id of the masthead', function(){
+    'use strict';
+
+    let element = $compile('<masthead masthead-id="the-id"></masthead>')(scope);
+    scope.$digest();
+
+    expect(element.find('header').attr('id')).toEqual('the-id');
+  });
+
+  it('should set showNav to true by default', function(){
+    'use strict';
+
+    let element = $compile('<masthead></masthead>')(scope);
+    scope.$digest();
+
+    expect(element.isolateScope().masthead.showNav).toEqual(true);
+  });
+
+  it('should set props on scope when passed in', function(){
+    'use strict';
+
+    scope.theNavValue = false;
+
+    let element = $compile('<masthead logo-id="the-logo-id" show-nav="theNavValue"></masthead>')(scope);
+    scope.$digest();
+
+    expect(element.isolateScope().masthead.showNav).toEqual(false);
+    expect(element.isolateScope().masthead.logoId).toEqual('the-logo-id');
+  });
+});
